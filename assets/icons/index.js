cb6495bf01cebfb39bbacc2539fe7375be01fbe9
ArJayEm Coding <raffmartinez14@gmail.com>
2022-10-23 01:44:56 +0800

    Add files via upload

---
+++ assets/icons/index.js
@@ -0,0 +1,9 @@
+export { default as facebookLogo } from "./facebook.ico";
+export { default as youtubeLogo } from "./youtube.ico";
+export { default as instagramLogo } from "./instagram.png";
+export { default as twitterLogo } from "./twitter.ico";
+export { default as tiktokLogo } from "./tiktok_circle.png";
+export { default as gankNowLogo } from "./ganknow.ico";
+export { default as cosplayManiaLogo } from "./cosplaymania_logo.jpg";
+export { default as pinterestLogo } from "./pinterest.png";
+export { default as imageLoading } from "./image-loading.png";
